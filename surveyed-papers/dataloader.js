b33d5f3a632fa2bf2f24bc30c4721cbac7187374
Amriteya Pandey <amriteyapandey@gmail.com>
2020-03-13 22:51:31 -0500

    Restructuring and refactoring

--- surveyed-papers/dataloader.js
+++ surveyed-papers/dataloader.js
@@ -1,41 +1,13 @@
-const Http = new XMLHttpRequest();
-const url =
-  "https://spreadsheets.google.com/feeds/cells/19TCHAcFTNa_Va6UankLiZiHxvwUmfXnJyJA7B5hsuIs/1/public/values?alt=json";
-Http.open("GET", url);
-Http.send();
-
-Http.onload = e => {
-  let dataStream = JSON.parse(Http.responseText).feed.entry;
-  fieldInitializer(dataStream, 55, 10); //ToDo: We have to dynamically insert the final file list count
-};
+fetchData().then(
+  data => fieldInitializer(data),
+  error => console.log(error)
+);
 
 //Initializing the field
 //Input consists of the data and the dimensions of the data
 //This function organizes data in a usable format.
 //The data wrangling will aim to organize information in a map, where each id will store the information related to an object
-function fieldInitializer(data, row, col) {
-  let inputArray = [];
-  let createdData = [];
-  let localCreatedData = {};
-
-  for (let i = 0; i < row; i++) {
-    if (Object.keys(localCreatedData).length != 0) {
-      createdData.push(localCreatedData);
-      localCreatedData = {};
-    }
-    for (let j = 0; j < col; j++) {
-      let cell = data[i * 10 + j]["gs$cell"];
-      let row = cell["row"];
-      let col = cell["col"];
-      if (row == 1) {
-        inputArray.push(cell["$t"]);
-      } else {
-        localCreatedData[inputArray[col - 1]] = cell["$t"];
-      }
-    }
-  }
-  //Setting all files data in the current session
-  createdData.push(localCreatedData);
+function fieldInitializer(createdData) {
   setAllFilesData(createdData);
   console.log(createdData);
 
---
+++ utils/fetchData.js
@@ -0,0 +1,52 @@
+function loadDataFromAPI() {
+  return new Promise(function(resolve, reject) {
+    const Http = new XMLHttpRequest();
+    const url =
+      "https://spreadsheets.google.com/feeds/cells/19TCHAcFTNa_Va6UankLiZiHxvwUmfXnJyJA7B5hsuIs/1/public/values?alt=json";
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onload = e => {
+      resolve(JSON.parse(Http.responseText).feed.entry);
+    };
+  });
+}
+
+/***
+ * Pulls data from the API and returns it wrapped within a Promise.
+ * How to use the data?
+ * 
+ * fetchData().then(data => console.log(data),
+ * error => console.log(error));
+ */
+async function fetchData() {
+  let data = await loadDataFromAPI();
+
+  //ToDo: We have to dynamically insert the final file list count
+  const row = 55;
+  const col = 10;
+
+  let inputArray = [];
+  let createdData = [];
+  let localCreatedData = {};
+
+  for (let i = 0; i < row; i++) {
+    if (Object.keys(localCreatedData).length != 0) {
+      createdData.push(localCreatedData);
+      localCreatedData = {};
+    }
+    for (let j = 0; j < col; j++) {
+      let cell = data[i * 10 + j]["gs$cell"];
+      let row = cell["row"];
+      let col = cell["col"];
+      if (row == 1) {
+        inputArray.push(cell["$t"]);
+      } else {
+        localCreatedData[inputArray[col - 1]] = cell["$t"];
+      }
+    }
+  }
+  //Setting all files data in the current session
+  createdData.push(localCreatedData);
+  return createdData;
+}
