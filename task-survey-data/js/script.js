269a3ca33d17727b99202320e6f92f826b59d3f7
aditeyapandey <aditeyapandey@gmail.com>
2020-03-20 13:26:54 -0400

    nesting code optimized and fixed

--- task-survey-data/js/script.js
+++ task-survey-data/js/script.js
@@ -89,82 +89,106 @@ function createSearchableTable(dataSet) {
  * @param {Object} data JSON data received from the proposed data sheet.
  */
 function generateTree(data) {
-  const syntax = {
-    name: ""
-  };
+  // const syntax = {
+  //   name: ""
+  // };
 
-  const root = {
-    name: "Target",
-    children: []
-  };
-  const attributeName = "Target";
-  recurseNode(root, attributeName, 0);
+  // const root = {
+  //   name: "Target",
+  //   children: []
+  // };
+  // const attributeName = "Target";
+  // recurseNode(root, attributeName, 0);
 
-  function recurseNode(root, attributeName, level, parentName, parentValue) {
-    for (let i = 0; i < data.length; i++) {
-      const currentData = data[i];
-      const targetName = data[i][attributeName];
-      if (level > 0 && currentData[parentName] !== parentValue) {
-        continue;
-      }
-      let filteredList = root.children.filter(
-        element => element.name === targetName
-      );
-      if (filteredList.length > 0 && level > 1) {
-        let targetChild = undefined;
+  // function recurseNode(root, attributeName, level, parentName, parentValue) {
+  //   for (let i = 0; i < data.length; i++) {
+  //     const currentData = data[i];
+  //     const targetName = data[i][attributeName];
+  //     if (level > 0 && currentData[parentName] !== parentValue) {
+  //       continue;
+  //     }
+  //     let filteredList = root.children.filter(
+  //       element => element.name === targetName
+  //     );
+  //     if (filteredList.length > 0 && level > 1) {
+  //       let targetChild = undefined;
+  //       targetChild = root.children.filter(x => x.name === targetName)[0];
 
-        targetChild = root.children.filter(x => x.name === targetName)[0];
+  //       if (level === 0) {
+  //         recurseNode(
+  //           targetChild,
+  //           "Specific Target", //Column name
+  //           1,
+  //           attributeName,
+  //           targetName
+  //         );
+  //       } else if (level === 1) {
+  //         recurseNode(
+  //           targetChild,
+  //           "Target Attribute",
+  //           2,
+  //           attributeName,
+  //           targetName
+  //         );
+  //       } else if (level === 2) {
+  //         targetChild.count = targetChild.count + 1;
+  //       }
+  //     } else if (filteredList.length === 0) {
+  //       const targetNode = JSON.parse(JSON.stringify(syntax));
+  //       targetNode.name = targetName;
+  //       if (level < 2) {
+  //         targetNode.children = [];
+  //       } else {
+  //         targetNode.count = 1;
+  //       }
 
-        if (level === 0) {
-          recurseNode(
-            targetChild,
-            "Specific Target",
-            1,
-            attributeName,
-            targetName
-          );
-        } else if (level === 1) {
-          recurseNode(
-            targetChild,
-            "Target Attribute",
-            2,
-            attributeName,
-            targetName
-          );
-        } else if (level === 2) {
-          targetChild.count = targetChild.count + 1;
-        }
-      } else if (filteredList.length === 0) {
-        const targetNode = JSON.parse(JSON.stringify(syntax));
-        targetNode.name = targetName;
-        if (level < 2) {
-          targetNode.children = [];
-        } else {
-          targetNode.count = 1;
-        }
+  //       root.children.push(targetNode);
 
-        root.children.push(targetNode);
+  //       if (level === 0) {
+  //         recurseNode(
+  //           targetNode,
+  //           "Specific Target",
+  //           1,
+  //           attributeName,
+  //           targetName
+  //         );
+  //       } else if (level === 1) {
+  //         recurseNode(
+  //           targetNode,
+  //           "Target Attribute",
+  //           2,
+  //           attributeName,
+  //           targetName
+  //         );
+  //       }
+  //     }
+  //   }
+  // }
 
-        if (level === 0) {
-          recurseNode(
-            targetNode,
-            "Specific Target",
-            1,
-            attributeName,
-            targetName
-          );
-        } else if (level === 1) {
-          recurseNode(
-            targetNode,
-            "Target Attribute",
-            2,
-            attributeName,
-            targetName
-          );
-        }
-      }
-    }
-  }
+  var nested_data = d3
+    .nest()
+    .key(function(d) {
+      return d["Target"];
+    })
+    .key(function(d) {
+      return d["Specific Target"];
+    })
+    .key(function(d) {
+      return d["Target Attribute"];
+    })
+    .rollup(function(leaves) {
+      return leaves.length;
+    })
+    .entries(data);
 
-  return root;
+  let targetVal = { name: "Target", children: nested_data };
+
+  var parsed = JSON.parse(JSON.stringify(targetVal), function(k, v) {
+    if (k === "key") this.name = v;
+    else if (k === "values") this.children = v;
+    else if (k === "value") this.count = v;
+    else return v;
+  });
+
+  return parsed;
 }
