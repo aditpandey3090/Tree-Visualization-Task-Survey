354a0d4e8736dbf025710d7f1248c09d456e75f8
aditeyapandey <aditeyapandey@gmail.com>
2020-03-20 12:23:31 -0400

    code with correct data parsing

--- js/utils/fetchData.js
+++ js/utils/fetchData.js
@@ -2,10 +2,10 @@
  * Calls Google spreadsheet URL to fetch data in the form of JSON.
  * This data is raw. The data can be found for every cell in the
  * spreadsheet and needs to be parsed.
- * 
+ *
  * For eg. https://spreadsheets.google.com/feeds/cells/19TCHAcFTNa_Va6UankLiZiHxvwUmfXnJyJA7B5hsuIs/1/public/values?alt=json
  * can be interpretted as
- * 
+ *
  * https://spreadsheets.google.com/feeds/cells/<docId>/<sheetId>/public/values?alt=jso
  * @param {String} docId The unique identifier of the document
  * @param {String} sheetId Incremental integer for the sheet.
@@ -13,8 +13,7 @@
 function loadDataFromAPI(docId, sheetId) {
   return new Promise(function(resolve, reject) {
     const Http = new XMLHttpRequest();
-    const url =
-      `https://spreadsheets.google.com/feeds/cells/${docId}/${sheetId}/public/values?alt=json`;
+    const url = `https://spreadsheets.google.com/feeds/cells/${docId}/${sheetId}/public/values?alt=json`;
     Http.open("GET", url);
     Http.send();
 
@@ -27,7 +26,7 @@ function loadDataFromAPI(docId, sheetId) {
 /***
  * Pulls data from the API and returns it wrapped within a Promise.
  * How to use the data?
- * 
+ *
  * fetchSurveyData().then(
  *  data => console.log(data),
  *  error => console.log(error)
@@ -61,8 +60,7 @@ function parseRawData(row, column, data) {
       let col = cell["col"];
       if (row == 1) {
         inputArray.push(cell["$t"].trim());
-      }
-      else {
+      } else {
         localCreatedData[inputArray[col - 1]] = cell["$t"];
       }
     }
@@ -74,13 +72,16 @@ function parseRawData(row, column, data) {
 
 async function fetchProposedData() {
   const docId = "1J66Qw0Db6Omv6jBecJpZPtQmeRnntpdoZHNfLpCD0Bo";
-  const sheetId = "2";
+  const sheetId = "3";
   let data = await loadDataFromAPI(docId, sheetId);
+
+  console.log(data);
 
   //ToDo: We have to dynamically insert the final file list count
   const row = 207;
-  const col = 12;
+  const col = 11;
 
   let createdData = parseRawData(row, col, data);
+  console.log(createdData);
   return createdData;
 }
