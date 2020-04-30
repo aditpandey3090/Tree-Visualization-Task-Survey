ae545cb4a643198f56db80e8bde0e38848338301
Amriteya Pandey <amriteyapandey@gmail.com>
2020-04-30 00:09:25 -0500

    Added actions tab in task tree

---
+++ task-tree/js/script.js
@@ -0,0 +1,20 @@
+// Tabs
+function openTab(evt, cityName) {
+  var i;
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  var activebtn = document.getElementsByClassName("testbtn");
+  for (i = 0; i < x.length; i++) {
+    activebtn[i].className = activebtn[i].className.replace(
+      " w3-dark-grey",
+      ""
+    );
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " w3-dark-grey";
+}
+
+var mybtn = document.getElementsByClassName("testbtn")[0];
+mybtn.click();
